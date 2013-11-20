f98e0abe1ad6769f2246f8e320fe1954b2e1bc95
giodif <gdifeterici@gmail.com>
2013-11-20 17:12:50 -0500

    Jacked up the templating again, derf. Fixed?

--- app/templates/index.php
+++ app/templates/index.php
@@ -1,5 +1,5 @@
 <?php include "inc/open.php"; ?>
                              
-        <h1><%= _.capitalize( siteName ) %></h1>
+        <h1><%= siteName %></h1>
                              
 <?php include "inc/close.php"; ?>
